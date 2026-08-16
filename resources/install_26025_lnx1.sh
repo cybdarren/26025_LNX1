063b935d937df5ba8bf998a25a5564f8fe26288e
Darren Wenn <darren.wenn@microchip.com>
2026-08-16 13:50:45 +0100

    Updated class directory name

--- resources/install_26025_lnx1.sh
+++ resources/install_26025_lnx1.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a60259f70e74b2918e82b0ad0646dc2fdad70945e6d79476b3a5fee1b7ff50d
-size 4156
+oid sha256:f1446165c413882ec45b25fbe8be33872a6aefa7833eb54a9bc31a48f82192b1
+size 4154
